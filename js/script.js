fbba80f9ae7d0d3ca3260d64170a94bbbbc3172a
Bruno Bretanha <bruno_bretanha@outlook.com>
2025-05-13 09:13:45 -0300

    finalizado ?

--- js/script.js
+++ js/script.js
@@ -10,10 +10,27 @@ const jump = () => {
 ) ;
 }
 
-const loop = setInterval( () => {
+ const loop = setInterval( () => {
     const pipePosition = pipe.offsetLeft;
+    const jumpmanPosition = +window.getComputedStyle(jumpman).bottom.replace('px' ,"");
+    /* esse + na frente converte de string para numero pois precisamos somente do numero e nao do px */
 
-    if (pipePosition <= 120)
-} , 10)
+    if (pipePosition <= 90 && pipePosition > 0 && jumpmanPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
+
+        jumpman.style.animation = 'none';
+        jumpman.style.bottom = `${jumpmanPosition}px`
+
+        jumpman.src = './image/game-over.png'
+        jumpman.style.width = '75px'
+        jumpman.style.marginleft ='50px'
+
+        clearInterval(loop);
+
+    }
+
+} , 10) 
 
 document.addEventListener(`keydown`, jump);
+  
